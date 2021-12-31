04054b347ae90af32459d876b335393ed8dbad41
Danilo-GB <rubendanilo.garcia@gmail.com>
2021-12-31 18:58:35 -0500

    Flex configured & Navigation start

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,26 @@
 <template>
-  <router-view />
+  <r-loader v-if="isLoading" />
+  <main class="flex flex-col-reverse h-screen md:flex-row">
+    <r-navigation class="bg-gray-600 h-1/6 md:w-1/6 md:h-screen" />
+    <router-view class="bg-gray-500 h-5/6 md:w-5/6 md:h-screen" />
+  </main>
 </template>
+
+<script>
+import RLoader from "@/components/layout/Loader.vue";
+import RNavigation from "@/components/layout/Navigation.vue";
+
+export default {
+  components: {
+    RLoader,
+    RNavigation,
+  },
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">HOME</div>
+  <div>HI RADIO</div>
 </template>
 
 <script>
