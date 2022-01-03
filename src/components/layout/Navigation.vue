66343240ad946a76a6e0b71ae571bfc70ea9cd6d
Danilo-GB <rubendanilo.garcia@gmail.com>
2022-01-02 20:12:09 -0500

    Station play feature

--- src/components/layout/Navigation.vue
+++ src/components/layout/Navigation.vue
@@ -52,7 +52,8 @@
     </div>
     <!-- SONG CONTROL-->
     <div class="w-1/5 md:w-full cursor-pointer">
-      <a>
+      <a @click="switchStationSate">
+        <audio :src="playingStation" autoplay></audio>
         <div class="logo-container">
           <svg
             v-if="isPlaying"
@@ -61,12 +62,16 @@
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
+              stroke="#0f0"
+              opacity="0.5"
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
               d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
             ></path>
             <path
+              stroke="#0f0"
+              opacity="0.5"
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
@@ -80,12 +85,16 @@
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
+              stroke="#f00"
+              opacity="0.3"
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
               d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
             ></path>
             <path
+              stroke="#f00"
+              opacity="0.3"
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
@@ -146,9 +155,15 @@ export default {
   },
   methods: {
     newStation(stationUrl) {
-      if (stationUrl) this.playingStation = stationUrl;
+      if (stationUrl) {
+        this.playingStation = stationUrl;
+        this.isPlaying = true;
+      }
     },
-    switchStationSate() {},
+    switchStationSate() {
+      this.isPlaying = false;
+      this.playingStation = "";
+    },
   },
   mounted() {
     bus.listen("changeStation", (data) => this.newStation(data));
