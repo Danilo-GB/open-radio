cfea315d537c15ffeec94e9cf2eaf74035379034
Danilo-GB <rubendanilo.garcia@gmail.com>
2022-01-04 15:49:43 -0500

    Add loader & catch error on images

--- src/components/Station.vue
+++ src/components/Station.vue
@@ -78,11 +78,29 @@
     </div>
     <!-- STATION IMAGE -->
     <img
+      v-show="!imageLoading"
       :src="station.favicon"
       alt="Oops, image not found."
       class="h-3/5 mx-auto text-center cursor-pointer"
+      @load="imageLoaded"
       @click="changeStation"
     />
+    <svg
+      v-if="imageLoading"
+      class="w-20 h-3/5 mx-auto"
+      fill="none"
+      stroke="#000"
+      opacity="0.5"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M18.364 5.636a9 9 0 010 12.728m0 0l-2.829-2.829m2.829 2.829L21 21M15.536 8.464a5 5 0 010 7.072m0 0l-2.829-2.829m-4.243 2.829a4.978 4.978 0 01-1.414-2.83m-1.414 5.658a9 9 0 01-2.167-9.238m7.824 2.167a1 1 0 111.414 1.414m-1.414-1.414L3 3m8.293 8.293l1.414 1.414"
+      ></path>
+    </svg>
     <!-- STATION NAME -->
     <div class="h-1/5 pt-1 text-center text-sm">
       {{ station.name }}
@@ -102,11 +120,12 @@ export default {
   data() {
     return {
       isFav: false,
+      imageLoading: true,
     };
   },
   methods: {
     changeStation() {
-      bus.fire("changeStation", this.station.url_resolved);
+      bus.fire("changeStation", this.station.url_resolved, this.station.name);
     },
     addToFavorites() {
       let favStations = [];
@@ -127,6 +146,9 @@ export default {
       }
 
       localStorage.setItem("fav-stations", JSON.stringify(favStations));
+    },
+    imageLoaded() {
+      this.imageLoading = false;
     },
   },
   created() {
